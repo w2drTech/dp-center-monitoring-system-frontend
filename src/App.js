3b2ff8b18ff6d683eca6af1a3461db66bbc269b7
Haritha <haritha@fedolab.com>
2023-10-05 11:18:48 +0530

    Created the top bar and fixed bugs in theme file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Topbar from "./scenes/global/Topbar";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
@@ -9,7 +10,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <main className="content">
+            <Topbar/>
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
